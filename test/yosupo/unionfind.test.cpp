e6ae463c120c7fbd3ce704c2a2a12a06762cd8d4
satanic <cflikethepsp@gmail.com>
2020-01-06 22:20:04 +0900

    add test for UnionFind

---
+++ test/yosupo/unionfind.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include <bits/stdc++.h>
+
+#include "../../data-structure/UnionFind.cpp"
+
+#define REP(a,b) for(int a=0;a<(int)(b);++a)
+
+signed main(){
+  int n, q;
+  std::cin >> n >> q;
+
+  UnionFind uf(n);
+  REP(i, q) {
+    int t, u, v;
+    std::cin >> t >> u >> v;
+    /* */if (t == 0) uf.unite(u, v);
+    else if (t == 1) std::cout << uf.same(u, v) << "\n";
+  }
+
+  return 0;
+}
