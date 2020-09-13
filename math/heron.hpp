1fa7891cd9d3e5ead0812236befe4b7b463e287f
satanic0258 <cflikethepsp@gmail.com>
2020-09-13 20:49:59 +0900

    [add] ヘロンの公式

---
+++ math/heron.hpp
@@ -0,0 +1,8 @@
+﻿// @brief ヘロンの公式
+// @shortcut heron
+// @description ヘロンの公式を用いて三角形の面積を計算する．O(1)．
+// @docs heron.md
+double Heron(double a, double b, double c) {
+ double s = (a + b + c) / 2;
+ return std::sqrt(s*(s - a)*(s - b)*(s - c));
+}
