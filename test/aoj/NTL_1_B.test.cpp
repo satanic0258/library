ec598abcaef409883185b757faf17472246532d4
satanic0258 <cflikethepsp@gmail.com>
2020-10-05 08:47:21 +0900

    [fix] ModIntのテスト

--- test/aoj/NTL_1_B.test.cpp
+++ test/aoj/NTL_1_B.test.cpp
@@ -1,8 +1,8 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_B"
 #include <bits/stdc++.h>
-#include "../../math/ModInt.hpp"
 
 constexpr int MOD = 1000000007;
+#include "../../math/ModInt.hpp"
 
 int main(){
 	ModInt m;
