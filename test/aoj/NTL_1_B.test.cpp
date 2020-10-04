159b554f1a54a175c7ac52eb074b8ae4ef591ff3
satanic0258 <cflikethepsp@gmail.com>
2020-10-05 08:31:30 +0900

    [add] ModIntのテスト

---
+++ test/aoj/NTL_1_B.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_B"
+#include <bits/stdc++.h>
+#include "../../math/ModInt.hpp"
+
+int main(){
+ ModInt m;
+ int n;
+ std::cin >> m >> n;
+ std::cout << (m ^ n) << std::endl;
+}
